312a14fb08b9c4949c081a6b6db6fc9ea13d7c5f
Optimocha <47341207+optimocha@users.noreply.github.com>
2020-07-16 01:23:04 +0300

    Update class-sbp-utils.php

--- includes/classes/class-sbp-utils.php
+++ includes/classes/class-sbp-utils.php
@@ -7,11 +7,6 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-// Security control for vulnerability attempts
-if ( ! defined( 'ABSPATH' ) ) {
-	die;
-}
-
 class SBP_Utils extends SBP_Abstract_Module {
 	public static function explode_lines( $text, $unique = true ) {
 		if ( '' === $text ) {
@@ -45,26 +40,4 @@ class SBP_Utils extends SBP_Abstract_Module {
 		return in_array( $path, get_option( 'active_plugins' ) );
 	}
 
-//	public static function on_plugin_update( $callback ) {
-//		add_action( 'upgrader_process_complete',
-//			function ( $upgrader_object, $hook_extra ) use ( $callback ) {
-//				$our_plugin = 'speed-booster-pack/speed-booster-pack.php';
-//				if ( $hook_extra['action'] == 'update' && $hook_extra['type'] == 'plugin' && isset( $hook_extra['plugins'] ) ) {
-//					if ( in_array( $our_plugin, $hook_extra['plugins'] ) ) {
-//						file_get_contents(SBP_CACHE_DIR . 'test1.txt', 'here');
-//						if ( is_callable( $callback ) ) {
-//							file_get_contents(SBP_CACHE_DIR . 'test2.txt', 'here');
-//							$arguments = func_get_args();
-//							file_get_contents(SBP_CACHE_DIR . 'test3.txt', 'here');
-//							array_shift( $arguments );
-//							file_get_contents(SBP_CACHE_DIR . 'test4.txt', 'here');
-//							$cuf = call_user_func_array( $callback, $arguments );
-//							file_get_contents(SBP_CACHE_DIR . 'testtest.txt', $cuf);
-//						}
-//					}
-//				}
-//			},
-//			10,
-//			2 );
-//	}
 }
