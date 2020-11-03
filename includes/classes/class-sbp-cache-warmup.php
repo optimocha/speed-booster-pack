a664f0a941ecb939bd61372a28260af659b971ad
Mustafa Zahid EFE <zhdefe@gmail.com>
2020-11-03 16:50:01 +0300

    Warmup cache update.

--- includes/classes/class-sbp-cache-warmup.php
+++ includes/classes/class-sbp-cache-warmup.php
@@ -75,7 +75,10 @@ class SBP_Cache_Warmup extends SBP_Abstract_Module {
 
 				if ( ! in_array( $href, $urls ) ) {
 					$urls[] = $href;
-					$this->warmup_process->push_to_queue( $href );
+					$this->warmup_process->push_to_queue( [ 'url' => $href ] );
+					if ( sbp_get_option( 'caching_separate_mobile' ) ) {
+						$this->warmup_process->push_to_queue( [ 'url' => $href, 'options' => ['user-agent' => 'Mobile'] ] );
+					}
 				}
 			}
 			$this->warmup_process->save()->dispatch();
--- includes/classes/class-sbp-warmup-process.php
+++ includes/classes/class-sbp-warmup-process.php
@@ -4,28 +4,44 @@ namespace SpeedBooster;
 
 class SBP_Warmup_Process extends \WP_Background_Process {
 	protected $action = 'warmup';
+	private $done = [];
+	private $success = [];
+	private $failed = [];
+	private $began = false;
 
 	protected function task( $item ) {
-		$response = wp_remote_get( $item, [
-			'blocking'            => false,
-			'compress'            => true,
-			'httpversion'         => '1.1',
+		$options      = isset( $item['options'] ) ? $item['options'] : [];
+		$args         = array_merge( [
+			'blocking'    => false,
+			'compress'    => true,
+			'httpversion' => '1.1',
 			'limit_response_size' => 100,
-		] );
+		], $options );
+
+		// TODO: Remove question marks and sharps from url
+		// TODO: Check if same url exists
+		$this->done[] = $item;
+
+		$response = wp_remote_get( $item['url'], $args );
 		if ( 200 !== wp_remote_retrieve_response_code( $response ) ) {
-			$transient = get_transient( 'sbp_warmup_errors' );
-			$errors    = is_array( $transient ) ? $transient : [];
-			/* translator: %s is the url of the page */
-			// BEYNTODO: Change Text
-			$errors['errors'][] = sprintf( __( 'Error occured while processing url: %s', 'speed-booster' ), $item );
+			$this->failed[] = $item;
+		} else {
+			$this->success[] = $item;
 		}
 
-		set_transient( 'sbp_warmup_started', true );
+		if ( $this->began === false ) {
+			set_transient( 'sbp_warmup_started', 1 );
+			$this->began = true;
+		}
 
 		return false;
 	}
 
 	protected function complete() {
+		/* translator: %s is the url of the page */
+		// BEYNTODO: Change Text
+		set_transient( 'sbp_warmup_errors', $this->failed );
+
 		set_transient( 'sbp_warmup_complete', true );
 		delete_transient( 'sbp_warmup_started' );
 		parent::complete();
