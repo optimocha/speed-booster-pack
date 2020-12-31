569a63c9e64e8f4da143036babaa7f9baec94187
Mustafa Zahid EFE <zhdefe@gmail.com>
2020-12-31 08:10:53 +0300

    Removed rel=preload from all styles but fonts.

--- includes/classes/class-sbp-critical-css.php
+++ includes/classes/class-sbp-critical-css.php
@@ -66,7 +66,6 @@ class SBP_Critical_CSS extends SBP_Abstract_Module {
 			if ( ! isset( $link->media ) || $link->media !== 'print' ) {
 				$link->media  = 'print';
 				$link->onload = "this.media='all'";
-				$link->outertext = '<link rel="preload" href="' . $link->href . '" as="style">' . PHP_EOL . $link->outertext;
 			}
 		}
 
