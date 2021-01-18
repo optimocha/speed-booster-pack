82b22ebffd75a49ddfa8aa833064d388a63d904b
Optimocha <47341207+optimocha@users.noreply.github.com>
2021-01-19 01:29:26 +0300

    Update pagespeed-tricker.php

--- includes/templates/pagespeed-tricker.php
+++ includes/templates/pagespeed-tricker.php
@@ -1,7 +1,7 @@
 <?php
 
 if( preg_match( '/Lighthouse/i', $_SERVER['HTTP_USER_AGENT'] ) ) {
-	echo "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"></head><body style=\"background:#0cce6b;font:bold 15vw sans-serif;color:#fff\">You have a perfect score. Good for you! Now go and speed up your website for real, actual people.</body></html>";
+	echo "<!DOCTYPE html><html lang=\"en\" style=\"background:#0cce6b;font:bold 8rem sans-serif;color:#fff\"><head><meta charset=\"UTF-8\"></head><body>You now have a perfect score. Good for you! Now go and optimize your website for humans. #Optimocha</body></html>";
 	exit;
 }
 
