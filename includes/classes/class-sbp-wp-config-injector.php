14d273cad2954dbb67172820347a00a43f024a15
Optimocha <47341207+optimocha@users.noreply.github.com>
2021-07-03 15:47:39 +0300

    Second hotfix

--- includes/classes/class-sbp-wp-config-injector.php
+++ includes/classes/class-sbp-wp-config-injector.php
@@ -55,7 +55,7 @@ class SBP_WP_Config_Injector {
 			$wp_config_file = dirname( ABSPATH ) . '/wp-config.php';
 		}
 
-		if ( $wp_filesystem->exists( $wp_config_file ) && sbp_check_file_permissions( $wp_config_file ) ) {
+		if ( $wp_filesystem->exists( $wp_config_file ) && $wp_filesystem->is_writable( $wp_config_file ) ) {
 			$wp_config_content = $wp_filesystem->get_contents( $wp_config_file );
 
 			if ( ! preg_match( '/\/\/ BEGIN SBP_WP_Config -' . SBP_VERSION . '-(.*?)\/\/ END SBP_WP_Config/si', $wp_config_content ) ) {
