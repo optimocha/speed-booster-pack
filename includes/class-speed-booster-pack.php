8a1b467db24306210bc42a38226ac09fa9426416
Optimocha <47341207+optimocha@users.noreply.github.com>
2022-04-09 14:11:29 +0300

    Update class-speed-booster-pack.php

--- includes/class-speed-booster-pack.php
+++ includes/class-speed-booster-pack.php
@@ -266,6 +266,9 @@ class Speed_Booster_Pack {
 	 * @access   private
 	 */
 	private function define_admin_hooks() {
+
+		if ( ! is_admin() ) { return; }
+		
 		$plugin_admin = new Speed_Booster_Pack_Admin( $this->get_plugin_name(), $this->get_version() );
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
