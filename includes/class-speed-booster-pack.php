e0035fe1ebb11d70705d94e781c6aa350dd7e535
Optimocha <47341207+optimocha@users.noreply.github.com>
2022-05-07 13:44:03 +0300

    Trying to fix white screen errors

--- includes/class-speed-booster-pack.php
+++ includes/class-speed-booster-pack.php
@@ -288,9 +288,9 @@ class Speed_Booster_Pack {
 		
 		$plugin_public = new Speed_Booster_Pack_Public( $this->get_plugin_name(), $this->get_version() );
 
-		$this->loader->add_action( 'template_redirect', $plugin_public, 'template_redirect', 0 );
+		$this->loader->add_action( 'init', $plugin_public, 'template_redirect', 1 );
 
-		$this->loader->add_action( 'shutdown', $plugin_public, 'shutdown', PHP_INT_MAX );
+		// $this->loader->add_action( 'shutdown', $plugin_public, 'shutdown', PHP_INT_MAX );
 
 		$this->loader->add_filter( 'wp_headers', $plugin_public, 'sbp_headers' );
 	}
