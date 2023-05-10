5cde3adb0e4005c4d45710d3e98e14f132b49aa4
Optimocha <47341207+optimocha@users.noreply.github.com>
2023-05-10 18:44:13 +0300

    tidying up notice-manager.php & adding PHPDoc

--- inc/features/notice-manager.php
+++ inc/features/notice-manager.php
@@ -5,14 +5,31 @@ namespace Optimocha\SpeedBooster\Features;
 defined( 'ABSPATH' ) || exit;
 
 class Notice_Manager {
+	/**
+	 * Number of notices.
+	 *
+	 * @since 5.0.0
+	 * @var int $notice_count
+	 */
 	public static $notice_count = 0;
-
+	
+	/**
+	 * Class constructor.
+	 *
+	 * @return void
+	 */
 	public function __construct() {
 		add_action( 'wp_ajax_sbp_dismiss_notice', [ $this, 'dismiss_notice' ] );
 		add_action( 'wp_ajax_sbp_remove_notice_transient', [ $this, 'remove_notice_transient' ] );
 		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
 	}
-
+	
+	/**
+	 * Dismisses a notice.
+	 *
+	 * @param  mixed $id
+	 * @return void
+	 */
 	public static function dismiss_notice( $id = null ) {
 		$is_ajax = false;
 		if ( $id == null ) {
@@ -31,7 +48,12 @@ class Notice_Manager {
 			}
 		}
 	}
-
+	
+	/**
+	 * Removes the transient of the notice.
+	 *
+	 * @return void
+	 */
 	public function remove_notice_transient() {
 		if ( isset( $_GET['action'] ) && $_GET['action'] == 'sbp_remove_notice_transient' && current_user_can( 'manage_options' ) ) { // Remove notice transient for one time processes like "cache cleared"
 			$id = $_GET['notice_id'];
@@ -41,13 +63,15 @@ class Notice_Manager {
 	}
 
 	/**
+	 * Displays a notice.
+	 *
+	 * If notice is like "Cache cleared" etc. set recurrent to true. If recurrent is true, notice manager will check transient.
+	 * 
 	 * @param $id
 	 * @param $text
 	 * @param string $type error|warning|success|info
 	 * @param bool $is_dismissible
 	 * @param string $notice_type one_time|recurrent|flash
-	 *
-	 * If notice is like "Cache cleared" etc. set recurrent to true. If recurrent is true, notice manager will check transient.
 	 */
 	public static function display_notice(
 		$id,
@@ -66,60 +90,82 @@ class Notice_Manager {
 			add_action( 'admin_notices',
 				function () use ( $type, $is_dismissible, $id, $text, $action, $pages, $notice_type ) {
 					self::$notice_count ++;
-
 					if ( $pages !== null && ! is_array( $pages ) ) {
 						$pages = [ $pages ];
 					}
-
 					if ( $pages !== null && get_current_screen() && ! in_array( get_current_screen()->id, $pages ) ) {
 						return;
 					}
-
 					echo '<div class="notice sbp-notice notice-' . $type . ' ' . ( $is_dismissible ? 'is-dismissible' : null ) . '" data-notice-action="' . $action . '" data-notice-id="' . $id . '">' . $text . '</div>';
-
 					if ( $notice_type == 'flash' ) {
 						delete_transient( $id );
 					}
 				} );
 		}
 	}
-
+	
+	/**
+	 * Checks if the notice should be displayed or not.
+	 *
+	 * @param  mixed $id
+	 * @return void
+	 */
 	public static function should_display( $id ) {
 		$dismissed_notices = self::get_dismissed_notices();
 
 		return ! in_array( $id, $dismissed_notices );
 	}
-
+	
+	/**
+	 * Gets dismissed notices from the _usermeta table.
+	 *
+	 * @return array
+	 */
 	public static function get_dismissed_notices() {
 		$dismissed_notices = get_user_meta( get_current_user_id(), 'sbp_dismissed_notices', true );
 
 		return is_array( $dismissed_notices ) ? $dismissed_notices : [];
 	}
-
+	
+	/**
+	 * Enqueues the inline JS code necessary to dismiss a notice.
+	 *
+	 * @return void
+	 */
 	public function enqueue_scripts() {
 		wp_add_inline_script( 'jquery',
 			'jQuery(document).on(\'click\', \'.sbp-notice .notice-dismiss, .sbp-notice .notice-dismiss-button\', function(e) {
 			if (jQuery(e.target).hasClass("notice-dismiss-button")) {
 				var notice_id = jQuery(this).data(\'notice-id\');
-	            var $notice = jQuery("div[data-notice-id=" + notice_id + "]");
-	            $notice.stop().slideUp();
-	        } else {
-	            var $notice = jQuery(this).parent();
-	            var notice_id = $notice.data(\'notice-id\');
+				var $notice = jQuery("div[data-notice-id=" + notice_id + "]");
+				$notice.stop().slideUp();
+			} else {
+				var $notice = jQuery(this).parent();
+				var notice_id = $notice.data(\'notice-id\');
             }
-		    var action = $notice.data(\'notice-action\');
-		    var data = {action: action, notice_id: notice_id};
-		    jQuery.get(ajaxurl, data);
+			var action = $notice.data(\'notice-action\');
+			var data = {action: action, notice_id: notice_id};
+			jQuery.get(ajaxurl, data);
 		});' );
 	}
-
+	
+	/**
+	 * Gets a number of notices.
+	 *
+	 * @return int
+	 */
 	public static function get_notice_count() {
 		return self::$notice_count;
 	}
-
+	
+	/**
+	 * Checks whether the notice is dismissed or not.
+	 *
+	 * @param  array $id
+	 * @return boolean
+	 */
 	public static function has_dismissed( $id ) {
 		$dismissed_notices = self::get_dismissed_notices();
-
 		return in_array( $id, $dismissed_notices );
 	}
 }
